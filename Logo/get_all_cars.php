2f4c1ae400dbd4aa953b03e27f97a734907899f8
kopeg1337 <igor.kopka2006@gmail.com>
2025-05-21 18:00:50 +0200

    c.d

--- Logo/get_all_cars.php
+++ Logo/get_all_cars.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; 
-$username = "root";
-$password = "";
-$dbname = "cardle";
+$servername = "sql109.infinityfree.com";
+$username = "if0_39043535";
+$password = "Cardle123 ";
+$dbname = "if0_39043535_cardle";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
